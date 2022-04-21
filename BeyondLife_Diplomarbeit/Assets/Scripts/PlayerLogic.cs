80a31ce03ddf6836f88965ce676a3c231ddcb962
Rudi Wagner <rudi.wagner62@gmail.com>
2022-04-21 15:18:20 +0200

    Added Player Movement

--- BeyondLife_Diplomarbeit/Assets/Scripts/PlayerLogic.cs
+++ BeyondLife_Diplomarbeit/Assets/Scripts/PlayerLogic.cs
@@ -7,47 +7,73 @@ using System.Collections.Generic;
 public class PlayerLogic : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer { get; private set; }
+
     public new Collider2D collider { get; private set; }
-    public MovementLogic movement { get; private set; }
+    public BoxCollider2D boxCollider { get; private set; }
+    public LayerMask wallLayer;
+    
+    public Rigidbody2D rigidBody;
+    public float speed = 5f;
+    
+    public Vector2 moveDirection { get; private set; }
+    public PlayerControls inputControls;
+    private InputAction move;
 
     public AnimatedSprite normal;
     public AnimatedSprite death;
+
+    
 
     private void Awake()
     {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
         this.collider = GetComponent<Collider2D>();
-        this.movement = GetComponent<MovementLogic>();
+        this.boxCollider = GetComponent<BoxCollider2D>();
+        this.inputControls = new PlayerControls();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        this.moveDirection = move.ReadValue<Vector2>();
+
+        //Horizontal Movement
+        this.rigidBody.velocity = new Vector2(this.moveDirection.x * speed, 0);
+
+        if(this.moveDirection.y >= 0.5f && checkIfGrounded())
+        {
+            //Vertical Movement (Gravity is always on --> only jumping)
+            this.rigidBody.velocity = new Vector2(this.moveDirection.x * speed, this.moveDirection.y * 1000f);
+        } else if(this.moveDirection.y <= -0.5f)
+        {
+            //Crouch
+        }
+    }
+
+    private bool checkIfGrounded()
+    {
+        return Physics2D.BoxCast(this.transform.position, new Vector2(4, 2), 0f, Vector2.down, 2f, this.wallLayer);
     }
 
     public void ResetState()
     {
         //Reset pacman to a "normal" state
-        this.movement.SetDirection(Vector2.zero);
         this.enabled = true;
         this.spriteRenderer.enabled = true;
         this.collider.enabled = true;
         this.gameObject.SetActive(true);
-        this.gameObject.transform.position = new Vector3(0, -8.5f, -5);
+        this.gameObject.transform.position = new Vector3(0, 0, 0);
     }
 
     public void deathAnimation()
     {
         //Play death animation after collision with a ghost
         this.collider.enabled = false;
-        this.movement.SetDirection(Vector2.zero);
         this.transform.rotation = Quaternion.AngleAxis(90 * Mathf.Rad2Deg, Vector3.forward);
         this.normal.enabled = false;
         this.normal.doLoop = false;
         this.death.enabled = true;
         this.death.Restart();
         this.death.doLoop = false;
-        this.movement.enabled = false;
         Invoke(nameof(die), 1.0f);
     }
 
@@ -60,16 +86,16 @@ public class PlayerLogic : MonoBehaviour
         this.normal.doLoop = true;
         this.death.enabled = false;
         this.death.doLoop = false;
-        this.movement.enabled = true;
     }
 
     private void OnEnable()
     {
-
+        this.move = this.inputControls.Player.Move;
+        this.move.Enable();
     }
 
     private void OnDisable()
     {
-        
+        this.move.Disable();
     }
 }
